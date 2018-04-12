8187e4d146d5079be0b1a2cf386b67dc892cdebd
RadOcta <H3Lz1997@gmail.com>
2018-04-12 18:57:11 +0100

    Update config.js

--- config.js
+++ config.js
@@ -3,3 +3,5 @@ exports.TOKEN = 'NDMwNjc5NzM1MTg2NzUxNDg4.DaTtUw.cQWijiFZtemSPqWWjfv-7VjE60Q';
 exports.PREFIX = 'y!';
 
 exports.GOOGLE_API_KEY = 'AIzaSyBeacgPpA6JTR3OBpiD_IIDWC-184xFz04';
+
+"image_folder": "./img",
