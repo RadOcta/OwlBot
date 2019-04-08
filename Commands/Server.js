f7ecc7b31860f61e238c5ef35eff360de647a68d
owl <owl@home-laptop.homenetwork>
2019-04-09 00:34:18 +0100

    Removed unneeded files

--- Commands/Server.js
+++
@@ -1,7 +0,0 @@
-// Server Commands
-module.exports = {
-    ping: function (client) {
-        const m = await msg.channel.send("Ping?");
-    		m.edit(`Pong! Latency is ${m.createdTimestamp - msg.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-    }
-}
